cad7c03929cf37fe8090063696510e9e55fbe505
EysteinnO <eyssi93@gmail.com>
2018-01-26 14:51:02 +0000

    Update verkefni2.py

--- verkefni2.py
+++ verkefni2.py
@@ -3,17 +3,7 @@ import os
 
 @route('/')
 def index():
-    return '''
-          <div>
-          <a href='/sida/sida1'>Sida 1</a> </b><a href='/sida/sida2'>Sida 2</a> <a href='/sida/sida3'>Sida 3</a>
-          </div>
-
-          <div>
-          <a href='/sida/mynd1'><img src='myndir/One.png'>
-          <a href='/sida/mynd2'><img src='myndir/Two.png'>
-          <a href='/sida/mynd3'><img src='myndir/Three.png'>          
-          </div>
-          '''
+    return "Prufa"
 
 #run()
 run(host='0.0.0.0', port=os.environ.get('PORT'))
