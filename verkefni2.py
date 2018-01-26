c9044714e740accf36454a558c291df0ddf4138d
EysteinnO <eyssi93@gmail.com>
2018-01-26 14:44:13 +0000

    Update verkefni2.py

--- verkefni2.py
+++ verkefni2.py
@@ -15,36 +15,5 @@ def index():
           </div>
           '''
 
-@route('/sida/<id>')
-def index(id):
-    if id == "sida1":
-        return "Her er ", id
-    if id == "sida2":
-        return "Her er ", id 
-    if id == "sida3":
-        return "Her er ", id
-    if id == "mynd1":
-        return "Her er mynd ", id, "<br><img src='../myndir/One.png'>"
-    if id == "mynd2":
-        return "Her er mynd ", id, "<br><img src='../myndir/Two.png'>"
-    if id == "mynd3":
-        return "Her er mynd ", id, "<br><img src='../myndir/Three.png'>"
-
-
-def page():
-    l = request.query.nr
-    
-@route('/gaman')
-def index():
-    x = request.query.tala
-    return "Þetta er gildi breytunnar tala", x
-
-@route('/myndir/<nafn>')
-def myndir_static(nafn):
-    return static_file(nafn,root='myndir')
-
-@error(404)
-def villa(error):
-    return "<br><h1>Þessi síða er ekki til</h1>"
 #run()
 run(host="0.0.0.0", port=os.environ.get('PORT'))
